f1a680644dc67542bab67698cef12f46620a9242
METrimble <kenzie.trimble@gmail.com>
2022-05-13 13:40:08 -0700

    made seperate files for data manipulation

---
+++ Adoption-Center-Data-Manipulation-Jude.sql
@@ -0,0 +1,19 @@
+--Data Manipulation for Adoption Center
+--Animal
+--Add Animal
+
+--Delete Animal
+
+--View bt name
+
+--Filter by Species
+
+--Show Animal Table
+
+---------------------------------------
+--Location
+--Add Location
+
+--Show Location Table
+
+---------------------------------------
