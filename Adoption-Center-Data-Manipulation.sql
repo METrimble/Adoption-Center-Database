38d1e64d647e21c66cd935e64c922f2294f8fe3f
METrimble <kenzie.trimble@gmail.com>
2022-05-13 11:05:33 -0700

    some chnages + new sql files to edit

---
+++ Adoption-Center-Data-Manipulation.sql
@@ -0,0 +1,31 @@
+--Data Manipulation for Adoption Center
+--Employee
+--Add Employee
+
+--Update Emplopyee
+
+--Show Employee Tables
+
+---------------------------------------
+--Animal
+--Add Animal
+
+--Delete Animal
+
+--View bt name
+
+--Filter by Species
+
+--Show Animal Table
+
+---------------------------------------
+--Location
+--Add Location
+
+--Show Location Table
+
+---------------------------------------
+--Foster Parent
+--Add Foster Parent 
+
+--Show Foster Parent Table
---
+++ Adoption-Center-dump.sql
@@ -0,0 +1,12 @@
+--Dump for Adoption Center Database 
+--Create for Employee
+
+--Create for Employee Animal
+
+--Create for Employee Location
+
+--Create for Animal
+
+--Create for Location
+
+--Create for Foster Parent 
