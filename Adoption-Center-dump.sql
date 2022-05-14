d6921a9458a8ce8a77e4bc8af267452eaef0bf09
METrimble <kenzie.trimble@gmail.com>
2022-05-13 18:06:32 -0700

    errors fixed, dump runs in sql now

--- Adoption-Center-dump.sql
+++ Adoption-Center-dump.sql
@@ -5,7 +5,7 @@ create table `employee`(
     `id` int(11) auto_increment,
     `first_name` varchar(255) not null,
     `last_name` varchar(255) not null, 
-    `type` enum('Volunteer', 'Part-time', 'Full-Time', 'Salaried') not null
+    `type` enum('Volunteer', 'Part-time', 'Full-Time', 'Salaried') not null,
     `pay_rate` int(11),
     `SSN` int(9),
     `email` varchar(254),
@@ -14,6 +14,53 @@ create table `employee`(
     `hiring_date` date not null, 
     primary key (`id`), 
     unique key `full_name` (`first_name`, `last_name`)
+);
+
+--Create for Location
+drop table if exists `location`;
+create table `location`(
+    `id` int(11) not null auto_increment,
+    `address` varchar(35) not null,
+    `city` varchar(28) not null,
+    `zip_code` varchar(10) not null,
+    `state` varchar(2) not null,
+    `sq_ft` int(11) not null,
+    `animal_in_rate` int(11) default 0,
+    PRIMARY KEY (`id`)
+);
+
+--Create for Foster Parent 
+drop table if exists `foster_parent`;
+create table `foster_parent`(
+    `id` int(11) auto_increment,
+    `first_name` varchar(255) not null, 
+    `last_name` varchar(255) not null,
+    `location_id` int(11) not null, 
+    `address` varchar(35) not null,
+    `city` varchar(28) not null,
+    `zip_code` varchar(10) not null, 
+    `state` varchar(2) not null,
+    primary key (`id`),
+    foreign key (`location_id`) references `location` (`id`),
+    unique key `full_name` (`first_name`, `last_name`)
+);
+
+--Create for Animal
+drop table if exists `animal`;
+create table `animal` (
+    `id` int(11) auto_increment,
+    `location_id` int(11) not null,
+    `foster_parent_id` int(11) not null, 
+    `animal_name`varchar(45),
+    `animal_species`enum('dog', 'cat', 'other') not null,
+    `animal_breed` varchar(45),
+    `animal_weight` decimal(6,4) not null,
+    `birthdate` date,
+    `spayed/neutered` tinyint not null,
+    `description` varchar(1000),
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`location_id`) REFERENCES `location` (`id`),
+    FOREIGN KEY (`foster_parent_id`) REFERENCES `foster_parent` (`id`) 
 );
 
 --Create for Employee Animal
@@ -41,58 +88,29 @@ insert into `employee`
 (`first_name`, `last_name`, `type`, `pay_rate`, `SSN`, `email`, `phone`, `hours_worked`, `hiring_date`) 
 values ('George', 'Bart', 'Volunteer', '0', '563889670', 'georgeb@gmail.com', '5097869123', '40', '2020-12-04');
 
-insert into `employee_animal`
-(`employee_id`, `animal_id`)
-values ((select `id` from `employee` where `first_name` = 'George' and `last_name` = 'Bart'), 1);
-
---Create for Animal
-drop table if exists `animal`;
-create table `animal` (
-    `id` int(11) unsigned not null auto_increment,
-    `location_id` int(11) not null,
-    `foster_parent_id` int(11) not null, 
-    `animal_name`varchar(45),
-    `animal_species`enum('dog', 'cat', 'other') not null,
-    `animal_breed` varchar(45),
-    `animal_weight` decimal(6,4) not null,
-    `birthdate` date,
-    `spayed/neutered` tinyint not null,
-    `description` varchar(1000),
-    PRIMARY KEY (`animal_id`),
-    FOREIGN KEY (`location_id`) REFERENCES `location` (`location_id`),
-    FOREIGN KEY (`foster_parent_id`) REFERENCES `foster_parent` (`foster_parent_id`) 
-) ENGINE=InnoDB AUTO_INCREMENT=201 DEFAULT CHARSET=utf8;
-
---Create for Location
-drop table if exists `location`;
-create table `location` (
-    `id` int(11) unsigned not null auto_increment,
-    `address` varchar(35) not null,
-    `city` varchar(28) not null,
-    `zip_code` varchar(10) not null,
-    `state` varchar(2), not null,
-    `sq_ft` int(11) not null,
-    `animal_in_rate` int(11) default 0
-    PRIMARY KEY (`location_id`),
-)
-
---Create for Foster Parent 
-drop table if exists `foster_parent`;
-create table `foster_parent`(
-    `id` int(11) auto_increment,
-    `first_name` varchar(255) not null, 
-    `last_name` varchar(255) not null,
-    `location_id` int(11) not null, 
-    `address` varchar(35) not null,
-    `city` varchar(28) not null,
-    `zip_code` varchar(10) not null, 
-    `state` varchar(2) not null,
-    primary key (`id`),
-    foreign key (`location_id`) references `location` (`id`),
-    unique key `full_name` (`first_name`, `last_name`)
-);
+--Sample Data for Location
+insert into `location` 
+(`address`, `city`, `zip_code`, `state`, `sq_ft`, `animal_in_rate`)
+values ('6785 SE Valley st', 'Portville', '87666', 'OR', '1500', '132');
 
 --Sample Data for Foster Parent 
 insert into `foster_parent`
 (`first_name`, `last_name`, `location_id`, `address`, `city`, `zip_code`, `state`)
 values ('Barbra', 'Stevens', 1, '2435 NE Treetop Lane', 'Eugene', '97450', 'OR');
+
+--Sample Data for animal
+insert into `animal`
+(`location_id`, `foster_parent_id`, `animal_name`, `animal_species`, `animal_breed`, `animal_weight`, `birthdate`, `spayed/neutered`, `description`)
+values (1, 1, 'Spoon', 'cat', 'Tabby', '10', '2020-04-15', '1', 'sweet boy');
+
+--Sample Data for employee-animal
+insert into `employee_animal`
+(`employee_id`, `animal_id`)
+values ((select `id` from `employee` where `first_name` = 'George' and `last_name` = 'Bart'), 1);
+
+--Sample Data for employee_location
+insert into `employee_location`
+(`employee_id`, `location_id`)
+values ((select `id` from `employee` where `first_name` = 'George' and `last_name` = 'Bart'), 1);
+
+
