4a597318e56b0927fc6d57ac722c9229c1b0eec9
Jude Williams <willijud@flip2.engr.oregonstate.edu>
2022-05-13 20:09:34 -0700

    Finished my part of DML, and added all DML to main file

--- Adoption-Center-Data-Manipulation-Jude.sql
+++ Adoption-Center-Data-Manipulation-Jude.sql
@@ -1,19 +1,29 @@
 --Data Manipulation for Adoption Center
 --Animal
 --Add Animal
+insert into `animal` (`location_id`, `foster_parent_id`, `animal_name`, `animal_species`, `animal_breed`, `animal_weight`, `birthdate`, `spayed/neutered`, `description`)
+values (:location_id, :foster_parent_id, :animal_namein, :animal_speciesin, :animal_breedin, :animal_weightin, :birthdatein, :spayed/neuteredin, :descriptionin);
 
 --Delete Animal
+delete * from `animal` where 'id' = :animal_idin;
 
 --View bt name
+select * from `animal` where 'animal_name' = :animal_namein;
 
 --Filter by Species
+select * from `animal` where 'animal_species' = :animal_speciesin;
 
 --Show Animal Table
+select * from `animal`
 
 ---------------------------------------
 --Location
 --Add Location
+insert into `location` (`address`, `city`, `zip_code`, `state`, `sq_ft`, `animal_in_rate`)
+values (:addressin, :city_in, :zip_codein, :statein, :sq_ftin, :animal_in_ratein);
+
 
 --Show Location Table
+select * from `location`;
 
 ---------------------------------------
