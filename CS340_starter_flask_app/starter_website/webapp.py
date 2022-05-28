9cfaa1aaf6fc34cc3e5081f5fa5cdaea06c18329
Jude Williams <willijud@flip1.engr.oregonstate.edu>
2022-05-27 17:29:25 -0700

    Changes to starter_website, changes to templates, changes to webapp.py

--- CS340_starter_flask_app/starter_website/webapp.py
+++ CS340_starter_flask_app/starter_website/webapp.py
@@ -1,24 +1,101 @@
+#This file is broadly adapted from the CS340_starter_flask_app that was provided to us
+#employee
+    #insert
+    #update
+    #delete
+#foster_parent
+    #insert
+    #update
+    #delete
+#animal
+    #insert
+    #update
+    #delete
+#location
+    #insert
+    #update
+    #delete
+
 from flask import Flask, render_template
 from flask import request, redirect
 from db_connector.db_connector import connect_to_database, execute_query
 #create the web application
 webapp = Flask(__name__)
 
-#provide a route where requests on the web application can be addressed
-@webapp.route('/hello')
-#provide a view (fancy name for a function) which responds to any requests on this route
-def hello():
-    return "Hello World!"
+#here's a basic function for understanding how flask works
+@webapp.route('/test')                                      #This part means when the webpage is routed to a page (in this case /test), we'll do whatever is under this
+def hello():                                                #This defines the function that we'll be doing (mostly a cosmetic thing)
+    print("/test activated...")                             #print statement so that you can look in the terminal for debugging
+    db_connection = connect_to_database()                   #set the variable db_connection to output from connect_to_database() function (we need this for later functions)
+    query = "SHOW TABLES;"                                  #set our query to whatever we want it to be
+    result = execute_query(db_connection, query).fetchall() #Get our result from the execute_query() function, using our db_connection and query vars, use fetchall()
+    print(result)                                           #print result to terminal for debugging
+    return render_template('test.html', rows=result)        #return render_template((page you want), rows=result) to render the page
 
-@webapp.route('/browse_bsg_people')
-#the name of this function is just a cosmetic thing
-def browse_people():
-    print("Fetching and rendering people web page")
+
+@webapp.route('/employees')
+def disp_employees():
+    print("/employees activated...")
     db_connection = connect_to_database()
-    query = "SELECT fname, lname, homeworld, age, id from bsg_people;"
+    query = "select * from `employee`;"
     result = execute_query(db_connection, query).fetchall()
     print(result)
-    return render_template('people_browse.html', rows=result)
+    return render_template('test.html', rows=result)   #UPDATE AND MATCH WITH EMPLOYEE.J2
+
+@webapp.route('/parents')
+def disp_parents():
+    print("/parents activated...")
+    db_connection = connect_to_database()
+    query = "select * from `foster_parent`;"
+    result = execute_query(db_connection, query).fetchall()
+    print(result)
+    return render_template('Foster_Parent.html', rows=result)
+
+@webapp.route('/animals')
+def disp_animals():
+    print("/animals activated...")
+    db_connection = connect_to_database()
+    query = "select * from `animal`;"
+    result = execute_query(db_connection, query).fetchall()
+    print(result)
+    return render_template('Animals.html', rows=result) 
+
+@webapp.route('/locations')
+def disp_locations():
+    print("/locations activated...")
+    db_connection = connect_to_database()
+    query = "select * from `location`;"
+    result = execute_query(db_connection, query).fetchall()
+    print(result)
+    return render_template('Locations.html', rows=result) 
+
+@webapp.route('/add_parent', methods=['POST','GET'])
+def add_parent():
+    print("/add_parent activated...")
+    db_connection = connect_to_database()
+    if request.method == 'GET':
+        query = "select * from `location`;"
+        result = execute_query(db_connection, query).fetchall()
+        print(result)
+        return render_template('Add_Foster_Parent.html', location = result)
+
+    elif request.method == 'POST':
+        print("...Adding new parent")
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        location = request.form['location']
+        address = request.form['address']
+        city = request.form['city']
+        zip_code = request.form['zip_code']
+        state = request.form['state']
+        query = 'insert into `foster_parent` (`first_name`, `last_name`, `location_id`, `address`, `city`, `zip_code`, `state`) values (%s, %s, %s, %s, %s, %s, %s);'
+        data = (first_name, last_name, location, address, city, zip_code, state)
+        execute_query(db_connection, query, data)
+        return ('Foster Parent added!')
+
+        
+
+
 
 @webapp.route('/add_new_people', methods=['POST','GET'])
 def add_new_people():
@@ -64,7 +141,7 @@ def home():
 def test_database_connection():
     print("Executing a sample query on the database using the credentials from db_credentials.py")
     db_connection = connect_to_database()
-    query = "SELECT * from bsg_people;"
+    query = "SELECT * FROM `location`;"
     result = execute_query(db_connection, query)
     return render_template('db_test.html', rows=result)
 
